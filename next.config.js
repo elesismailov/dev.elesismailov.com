8ce54f7162d33e68e8717896cd69c3f7777df3a4
Eles <eleswastaken@gmail.com>
2023-03-29 14:03:00 +0300

    Set up the project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['ru', 'en',],
+    defaultLocale: 'ru',
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+
 }
 
 module.exports = nextConfig
