0d33e02abefc42fd3921f1cb7a534f651fb93f33
eles <eleswastaken@gmail.com>
2023-04-04 12:18:55 +0300

    Add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -9,7 +10,18 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet" />
         <link rel="icon" href="https://elesismailov.com/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script id="google-tag" strategy="afterInteractive" async src="https://www.googletagmanager.com/gtag/js?id=G-WVF6W872VG"></Script>
+        <Script id="google-tag-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-WVF6W872VG');
+            console.log('Google Analytics plugged in...')
+          `}
+        </Script>
       </Head>
       <body>
         <div id="overflow-wrapper">
