1862c7708b58010fda3628c19ecda9a9de6fa925
Eles Ismailov <elesismailov@Eless-Laptop.local>
2024-05-24 12:43:29 +0200

    Rewrite hero section tailwind

--- pages/index.js
+++ pages/index.js
@@ -14,53 +14,56 @@ import ProjectsSection from '@/components/ProjectsSection';
 
 export default function Home() {
 
-  const router = useRouter();
-  const locale = router.locale;
-  const data = index_data.locales[locale] || index_data.locales['en'];
+    const router = useRouter();
+    const locale = router.locale;
+    const data = index_data.locales[locale] || index_data.locales['en'];
 
-  if (locale) {
-    return (
-      <>
-        <Head>
-          <title>Eles Ismailov Web Developer</title>
-          <meta name="description" lang="en" content="Eles Ismailov Web Developer" />
-        </Head>
-
-
-        <Header data={data} />
-
-        <main lang={locale}>
-
-          <section className="intro">
-            <div className="wrapper">
-              <h1 dangerouslySetInnerHTML={{ __html: data.h1_1 }}></h1>
-            </div>
-            <hr />
-          </section>
-
-          <section className="recent-works">
-            <h2>{data.recent_works.h2}</h2>
-            
-            <div className="wrapper">
-              <ProjectsSection data={data.recent_works.projects} />
-            </div>
-
-            <Link href="/projects" locale={locale} className="projects-cta">{data.recent_works.cta}</Link>
-
-            <hr className='works-seperator'/>
-          </section>
+    if (locale) {
+        return (
+            <>
+                <Head>
+                    <title>Eles Ismailov Web Developer</title>
+                    <meta name="description" lang="en" content="Eles Ismailov Web Developer" />
+                </Head>
 
 
-          <HireMe data={data.hire_me} />
+                <Header data={data} />
 
-        </main>
+                <main lang={locale} className=''>
+
+                    <section className="hero-section">
+                        <div className="wrapper wrapper wrapper max-w-screen-2xl mx-auto h-[95vh] flex items-center pl-56">
+                            <h1 className="text-9xl font-normal leading-none">
+                                I am <span className='text-[#F23737]'>Eles</span>.<br/>Create.<br/>Build.
+                            </h1>
+                        </div>
+                    </section>
+
+                    
+
+                    <section className="recent-works">
+                        <h2>{data.recent_works.h2}</h2>
+
+                        <div className="wrapper">
+                            <ProjectsSection data={data.recent_works.projects} />
+                        </div>
+
+                        <Link href="/projects" locale={locale} className="projects-cta">{data.recent_works.cta}</Link>
+
+                        <hr className='works-seperator' />
+                    </section>
 
 
-        <Footer data={data} />
-      </>
-    )
-  } else {
-    return <NoLocale />
-  }
+                    <HireMe data={data.hire_me} />
+
+                </main>
+
+
+                <Footer data={data} />
+            </>
+        )
+    } else {
+        return <NoLocale />
+    }
 }
 
